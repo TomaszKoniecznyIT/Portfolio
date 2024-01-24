cbab2345998702b3c15b4b9cf8a75dcfc277345d
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-01-24 11:58:25 +0000

    adding custom navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import * as React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from "../components/CustomNavbar";
 
 export default function Home() {
   return (
     <>
+      <CustomNavbar />
       <div>Hello world!</div>
     </>
   );
