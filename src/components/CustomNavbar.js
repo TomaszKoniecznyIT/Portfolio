a5afe825f0574215b9ee6d76be232dc40a239d1c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-01-25 11:51:55 +0000

    adding page contact and projects

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -11,7 +11,9 @@ function CustomNavbar() {
           <Navbar.Brand href="/">Tomasz Konieczny</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
