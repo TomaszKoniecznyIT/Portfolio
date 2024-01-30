79323551e2ed5baf1618a639a918fc46d7994eee
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-01-30 16:17:45 +0000

    adding contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 
 export default function ContactForm() {
-  return <div>form</div>;
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="name" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+  );
 }
