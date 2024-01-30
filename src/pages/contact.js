d4f7ee1975195217342602cb4bba297f04a0aab6
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-01-30 11:42:32 +0000

    adding contact content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,17 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div>
+        <h1>Contact me</h1>
+        <p>
+          I'm glad you visited my portfolio. I'll be happy to answer your
+          questions!
+        </p>
+      </div>
+      <ContactForm />
+    </>
+  );
 }
