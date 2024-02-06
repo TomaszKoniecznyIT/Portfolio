45e13540b4a4c4b99eccae93073c5a46e9162dd9
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-06 13:39:44 +0000

    adding AboutCard

---
+++ src/components/AboutCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import image from "../../static/images/bg23.png";
+
+export default function AboutCard() {
+  return (
+    <Card className="mx-auto my-5" style={{ width: "70vw" }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>About</Card.Title>
+        <Card.Text>
+          I am a husband and a proud father of one son, who is currently
+          pursuing a degree in mathematics at the university, continuing a
+          family tradition. I have a passion for cycling and a keen interest in
+          both automotive and computer technologies.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import AboutCard from "../components/AboutCard";
 
 export default function About() {
   return (
     <Container>
       <Row>
-        <Col>About</Col>
+        <Col>
+          <AboutCard />
+        </Col>
       </Row>
       <Row>
         <Col>Education</Col>
