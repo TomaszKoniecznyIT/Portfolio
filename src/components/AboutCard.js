944f7311ad51d321d25b0e8a0bc80d4497fa902c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-07 08:38:31 +0000

    color change for card title

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -7,12 +7,12 @@ export default function AboutCard() {
     <Card border="warning">
       <Card.Img variant="top" src={image} />
       <Card.Body>
-        <Card.Title>Motto</Card.Title>
+        <Card.Title className="text-warning">Motto</Card.Title>
         <Card.Text>
           "Dreams are realized through the consistent dedication and resilience
           of hard work."
         </Card.Text>
-        <Card.Title>Privately</Card.Title>
+        <Card.Title className="text-warning">Privately</Card.Title>
         <Card.Text>
           I am a husband and a proud father of one son, who is currently
           pursuing a degree in mathematics at the university, continuing a
