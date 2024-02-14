07aeaa629d976071a583c51bdb8cc3d63e42ed3c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-14 20:34:41 +0000

    adding card university

--- src/components/University.js
+++ src/components/University.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Card, CardGroup, CardText } from "react-bootstrap";
+import image1 from "../../static/images/university.png";
+import image2 from "../../static/images/uz_logo.jpg";
+import { Link } from "gatsby";
 
 export default function University() {
-  return <div>University</div>;
+  return (
+    <CardGroup className="my-5">
+      <Card border="warning">
+        <Card.Img src={image1} />
+      </Card>
+      <Card border="warning">
+        <Link to="https://uz.zgora.pl/en/">
+          <Card.Img variant="top" src={image2} />
+        </Link>
+        <CardText>
+          I have obtained a Master's degree in Mathematics with a specialization
+          in Mathematical and Computational Methods in Economics.
+        </CardText>
+      </Card>
+    </CardGroup>
+  );
 }
--- src/pages/about/education.js
+++ src/pages/about/education.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Technical from "../../components/Technical";
+import University from "../../components/University";
 
 export default function education() {
-  return <div>Education</div>;
+  return (
+    <Container>
+      <Row>
+        <University />
+      </Row>
+      <Row>
+        <Technical />
+      </Row>
+    </Container>
+  );
 }
