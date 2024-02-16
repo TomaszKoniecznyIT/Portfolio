7d4ec99ef550cb2885c9f60719eab72cbf2748bb
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-16 22:42:23 +0000

    adding a few more names with the logo to hard skills

--- src/components/HardSkills.js
+++ src/components/HardSkills.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Card, Image } from "react-bootstrap";
-import logo from "../../static/images/js.png";
+import { Card, Image, Row } from "react-bootstrap";
+import logoJS from "../../static/images/js.png";
+import logoReact from "../../static/images/react.png";
+import logoPython from "../../static/images/python.png";
+import logoFlask from "../../static/images/flask.png";
+
+const skillsData = [
+  { id: 1, name: "Java Script", logo: logoJS },
+  { id: 2, name: "React", logo: logoReact },
+  { id: 2, name: "Python", logo: logoPython },
+  { id: 2, name: "", logo: logoFlask },
+  { id: 2, name: "React4", logo: logoJS },
+  { id: 2, name: "React5", logo: logoJS },
+];
 
 export default function HardSkills() {
   return (
-    <Card style={{ width: "8rem" }}>
-      <Image src={logo} />
-      <Card.Title>Java Script</Card.Title>
-    </Card>
+    <Row>
+      {skillsData.map((skill) => (
+        <Card key={skill.id} style={{ width: "8rem", margin: "10px" }}>
+          <Image src={skill.logo} />
+          <Card.Title>{skill.name}</Card.Title>
+        </Card>
+      ))}
+    </Row>
   );
 }
