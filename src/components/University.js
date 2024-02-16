53f2f3845b119eb8ef7f4869f7ab7f8ac64d3a57
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-16 12:29:38 +0000

    adding information about studies

--- src/components/University.js
+++ src/components/University.js
@@ -14,9 +14,15 @@ export default function University() {
         <Link to="https://uz.zgora.pl/en/">
           <Card.Img variant="top" src={image2} />
         </Link>
-        <CardText>
+        <CardText className="my-2 mx-3">
           I have obtained a Master's degree in Mathematics with a specialization
           in Mathematical and Computational Methods in Economics.
+        </CardText>
+        <CardText className="my-2 mx-3">
+          For five years in our classes, we focused mainly on mathematical
+          topics with a significant emphasis on algorithms. We also had numerous
+          technical sessions involving the use of computers, enabling us to
+          translate theoretical knowledge into practical skills.
         </CardText>
       </Card>
     </CardGroup>
