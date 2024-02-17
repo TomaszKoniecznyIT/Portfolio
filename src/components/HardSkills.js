3f2c23cfc2f9d2c7fafa6db01890d0aa07f364ab
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-17 12:58:31 +0000

    adding more items to skillsData

--- src/components/HardSkills.js
+++ src/components/HardSkills.js
@@ -4,14 +4,16 @@ import logoJS from "../../static/images/js.png";
 import logoReact from "../../static/images/react.png";
 import logoPython from "../../static/images/python.png";
 import logoFlask from "../../static/images/flask.png";
+import logoCSS from "../../static/images/css.png";
+import logoHTML from "../../static/images/html.png";
 
 const skillsData = [
   { id: 1, name: "Java Script", logo: logoJS },
   { id: 2, name: "React", logo: logoReact },
-  { id: 2, name: "Python", logo: logoPython },
-  { id: 2, name: "", logo: logoFlask },
-  { id: 2, name: "React4", logo: logoJS },
-  { id: 2, name: "React5", logo: logoJS },
+  { id: 3, name: "HTML", logo: logoHTML },
+  { id: 4, name: "CSS", logo: logoCSS },
+  { id: 5, name: "Python", logo: logoPython },
+  { id: 6, name: "", logo: logoFlask },
 ];
 
 export default function HardSkills() {
