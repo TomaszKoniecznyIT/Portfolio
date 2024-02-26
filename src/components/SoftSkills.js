bf3a6edf6183694eed44a97549a7e5b33de72be6
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-02-26 18:54:39 +0000

    adding more soft skills

--- src/components/SoftSkills.js
+++ src/components/SoftSkills.js
@@ -4,11 +4,17 @@ import { Card, Container, Image, Row } from "react-bootstrap";
 import pictureCommunication from "../../static/images/communication.png";
 import pictureProblemSolving from "../../static/images/problem_solving.png";
 import pictureTeamwork from "../../static/images/teamwork.png";
+import pictureAdaptability from "../../static/images/adaptability.png";
+import pictureTimeManagement from "../../static/images/time_management.png";
+import pictureCriticalThinking from "../../static/images/critical_thinking.png";
 
 const skillsData = [
   { id: 1, name: "Communication skills", picture: pictureCommunication },
   { id: 2, name: "Problem-solving", picture: pictureProblemSolving },
   { id: 3, name: "Teamwork", picture: pictureTeamwork },
+  { id: 4, name: "Adaptability", picture: pictureAdaptability },
+  { id: 5, name: "Time Management", picture: pictureTimeManagement },
+  { id: 6, name: "Critical Thinking", picture: pictureCriticalThinking },
 ];
 
 export default function SoftSkills() {
