1e8750d22963aced33ee9e08375563994ccb79a5
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-03-01 14:27:25 +0000

    adding game component

--- src/components/BallsGame.js
+++ src/components/BallsGame.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
 
 export default function BallsGame() {
-  return <div>BallsGame</div>;
+  return (
+    <Card>
+      <Card.Title>Balls Game</Card.Title>
+      <Card.Text>
+        I embarked on my coding journey with Python. Once I had mastered the
+        fundamentals, I started tackling challenges on platforms like Codewars.
+        To diversify my coding practice, I decided to create a fully functional
+        application that could be used interactively. However, I was lacking
+        inspiration. It was during a moment spent beside my wife, who was
+        engrossed in a simple logic game on her laptop, that an idea struck me:
+        could I, after just a few months of learning Python, create a duplicate
+        of such a game? This sparked the inception of my first independent
+        project.{" "}
+      </Card.Text>
+    </Card>
+  );
 }
