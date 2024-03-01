029bd049ca03edbd8b0321c661f929c61d025d1b
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-03-01 13:50:27 +0000

    adding project components

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import BallsGame from "../components/BallsGame";
+import TGB from "../components/TGB";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <Container>
+      <Row className="my-5">
+        <BallsGame />
+      </Row>
+      <Row className="my-5">
+        <TGB />
+      </Row>
+    </Container>
+  );
 }
