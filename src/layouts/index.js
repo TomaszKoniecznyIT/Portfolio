94a18b08dc01e7dd75fec0b5af02f0cf7c77a505
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-03-08 12:23:58 +0000

    adding a logo to the footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+import { Card, Image } from "react-bootstrap";
 import CustomNavbar from "../components/CustomNavbar";
+import github from "../../static/images/github.png";
+import linkedIn from "../../static/images/linkedIn.png";
+import codewars from "../../static/images/codewars.png";
 
 export default function Layout({ children }) {
   return (
@@ -9,6 +12,9 @@ export default function Layout({ children }) {
       {children}
       <Card.Footer>
         <small className="text-muted">Copyright 2024 Tomasz Konieczny</small>
+        <Image src={linkedIn} style={{ width: "2rem" }} />
+        <Image src={github} style={{ width: "2rem" }} />
+        <Image src={codewars} style={{ width: "2rem" }} />
       </Card.Footer>
     </Card>
   );
