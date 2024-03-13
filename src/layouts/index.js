3bab65ea295935a3f9db3d3c9decb6a3f0c5dda6
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-03-13 09:03:02 +0000

    adding links to icons in the footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import CustomNavbar from "../components/CustomNavbar";
 import github from "../../static/images/github.png";
 import linkedIn from "../../static/images/linkedIn.png";
 import codewars from "../../static/images/codewars.png";
+import { Link } from "gatsby";
 
 export default function Layout({ children }) {
   return (
@@ -14,9 +15,18 @@ export default function Layout({ children }) {
         <small className="text-muted mx-5">
           Copyright 2024 Tomasz Konieczny
         </small>
-        <Image src={linkedIn} style={{ width: "2rem", marginRight: "1rem" }} />
-        <Image src={github} style={{ width: "2rem", marginRight: "1rem" }} />
-        <Image src={codewars} style={{ width: "2rem" }} />
+        <Link to="https://www.linkedin.com/in/tomasz-konieczny-176797289/">
+          <Image
+            src={linkedIn}
+            style={{ width: "2rem", marginRight: "1rem" }}
+          />
+        </Link>
+        <Link to="https://github.com/TomaszKoniecznyIT">
+          <Image src={github} style={{ width: "2rem", marginRight: "1rem" }} />
+        </Link>
+        <Link to="https://www.codewars.com/users/mltk12">
+          <Image src={codewars} style={{ width: "2rem" }} />
+        </Link>
       </Card.Footer>
     </Card>
   );
